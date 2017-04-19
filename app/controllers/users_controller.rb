78b7dbf15bb202185767192ae9e0fa9ac2bd6e3e
Yoan Grandjacques <yoan.grandjacques@icloud.com>
2017-04-19 21:47:51 +0000

    starting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
     
+    before_action :logged_in_user, only: [:index, :edit, :update]
+    before_action :correct_user, only: [:edit, :update]
+    
     def show
         @user = User.find(params[:id])
     end
@@ -11,16 +14,48 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            log_in @user
+            flash[:success] = "Welcome to the Sample App !"
             redirect_to @user
         else
             render 'new'
         end
     end
     
+    def edit
+       @user = User.find(params[:id]) 
+    end
+    
+    def update
+        if @user.update_attributes(user_params)
+            flash[:success] = "Profile updated"
+            redirect_to @user
+        else
+            render |'edit'
+        end
+    end
+    
+    def index
+       @users = User.paginate(page: params[:page]) 
+    end
+    
     private
     
     def user_params
         params.require(:user).permit(:name, :surname, :age, :email, :home_address, :password, :password_confirmation)
+    end
+    
+    def logged_in_user
+        unless logged_in?
+            store_location
+            flash[:danger] = "Please log in."
+            redirect_to login_url
+        end
+    end
+    
+    def correct_user
+       @user = User.find(params[:id])
+       redirect_to(root_url) unless @user == current_user
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   post   'signup'    => 'users#create'
   get    'login'     => 'sessions#new'
   post   'login'     => 'sessions#create'
-  delete 'logout'    => 'sessions#destroy'
+  get 'logout'    => 'sessions#destroy'
   resources :users
 end
